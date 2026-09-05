bd5c764b1a5b9f2888f8e452768ddc8602e25a08
Akash R <akashrajenthiran73@gmail.com>
2026-09-05 12:26:01 +0530

    Fix captcha

--- auth/captcha.php
+++ auth/captcha.php
@@ -1,11 +1,13 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-// Generate 5-character random string
 $captcha_code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 5);
-$_SESSION['captcha'] = $captcha_code;
 
-// Create image
+// login_process.php ethirpaarkura exact session variable name!
+$_SESSION['captcha_code'] = $captcha_code;
+
 $image = imagecreatetruecolor(120, 40);
 $background = imagecolorallocate($image, 240, 240, 240);
 $text_color = imagecolorallocate($image, 30, 30, 30);
@@ -13,12 +15,10 @@ $line_color = imagecolorallocate($image, 200, 200, 200);
 
 imagefill($image, 0, 0, $background);
 
-// Add noise lines
 for ($i = 0; $i < 4; $i++) {
     imageline($image, rand(0, 120), rand(0, 40), rand(0, 120), rand(0, 40), $line_color);
 }
 
-// Render string without external fonts (built-in font 5)
 imagestring($image, 5, 35, 12, $captcha_code, $text_color);
 
 header("Content-Type: image/png");
